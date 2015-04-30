74d2a7b638b26cfae80a8d771bcb30fc640fbb92
Nicolas Wormser <nicolas@cybercrumbs.net>
2015-04-30 17:31:46 +0200

    Build html and scss to public folder.

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,11 @@ var config = {
 	port: 8888,
 	host: 'localhost',
   paths: {
-    source: '/app',
-    public: '/public',
-    css: '',
-    sass: '',
-    js: '',
-    images: '/images',
+    source: './app',
+    public: './public',
+    sass: "./app/**/*.{sass,scss}",
+    html: "./app/**/*.html",
+    images: './app/images',
   },
 };
 
@@ -27,7 +26,9 @@ var site = require('./package.json'); // store package.json data in site variabl
 var gulp = require('gulp');
 var del = require('del');
 var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
 var ghPages = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
 
 
 /*************************************************
@@ -36,8 +37,8 @@ var ghPages = require('gulp-gh-pages');
 
 gulp.task('default', ['build:development']);
 
-gulp.task('build:development', function(cb) {
-  runSequence('clean', 'build');
+gulp.task('build:development', function() {
+  runSequence('clean', 'build', 'server');
 });
 
 gulp.task('clean', function (cb) {
@@ -46,13 +47,31 @@ gulp.task('clean', function (cb) {
   ], cb);
 });
 
-gulp.task('build', function (cb) {
-  del([
-    // TODO Fix
-  ], cb);
-});
+gulp.task('build', ['html', 'sass']);
 
 gulp.task('deploy', function() {
-  return gulp.src('./' + config.paths.public + '/**/*')
+  return gulp.src(config.paths.public + '/**/*')
     .pipe(ghPages());
+});
+
+gulp.task('html', function(){
+
+  return gulp.src(config.paths.html)
+    .pipe(gulp.dest(config.paths.public))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('sass', function(){
+	return gulp.src(config.paths.sass)
+		.pipe(sass())
+		.pipe(gulp.dest(config.paths.public + '/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: config.paths.public
+    }
+  });
 });
